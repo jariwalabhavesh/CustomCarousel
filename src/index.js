b9857b6d52aa4037ba5ad0f60b612917b61871fc
Bhavesh Jariwala <jariwalabhavesh30@gmail.com>
2020-03-15 16:15:01 +0530

    finding out total no of slides and keeping track of current slide on swiping.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Dimensions } from 'react-native'
 import styles from './style'
 import { cardPerSlide } from './config'
 import CarouselSlide from './components/Slide'
 import movies from './data'
 
+const { width: screenWidth } = Dimensions.get('window')
+
 export default class MovieHome extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      totalSlide: 0,
+      currentSlide: 1
+    }
+  }
+
+  onNext = () => {
+
+  }
+
+  onPrev = () => {
+
+  }
+
+  setTotalSlides = (contentWidth) => {
+    const { totalSlide } = this.state
+    if (contentWidth !== 0) {
+      const approxSlide = contentWidth / screenWidth
+      if (totalSlide !== parseInt(approxSlide)) {
+        this.setState({
+          totalSlide: parseInt(Math.ceil(approxSlide.toFixed(2)))
+        })
+      }
+    }
+  }
+
+  setCurrentSlide = (currentSlide) => {
+    this.setState({
+      currentSlide
+    })
+  }
+
+
+  handleScrollEnd = (e) => {
+    if (!e) {
+      return
+    }
+    const { nativeEvent } = e
+    if (nativeEvent && nativeEvent.contentOffset) {
+      let currentSlide = 1
+      if (nativeEvent.contentOffset.x === 0) {
+        this.setCurrentSlide(currentSlide)
+      } else {
+        const approxCurrentSlide = nativeEvent.contentOffset.x / screenWidth
+        currentSlide = parseInt(Math.ceil(approxCurrentSlide.toFixed(2)) + 1)
+        this.setCurrentSlide(currentSlide)
+      }
+    }
+  }
+
   render() {
     const noOfSlides = Math.ceil(movies.length / cardPerSlide)
     return (
@@ -21,7 +77,7 @@ export default class MovieHome extends Component {
           showsHorizontalScrollIndicator={false}
           decelerationRate={0}
           snapToAlignment={'center'}
-          onContentSizeChange={this.setTotalPage}
+          onContentSizeChange={this.setTotalSlides}
           onMomentumScrollEnd={this.handleScrollEnd}
         >
           {[...Array(noOfSlides)].map((e, i) => {
